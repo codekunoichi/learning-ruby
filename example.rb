579c14dbaf81e8a6341ca05d3ae2e4388baa1d72
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-17 22:32:04 -0800

    gets.chomp example

--- example.rb
+++ example.rb
@@ -1,1 +1,11 @@
 puts "Hello World!"
+
+print "Please input your string:"
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+   puts user_input.gsub!(/s/, "th")
+else
+   puts "No ss found!"
+end
