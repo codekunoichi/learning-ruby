6afdb36d40db396625d7cee0bb530dfda534aac7
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-19 18:49:33 -0800

    Sting interpolation example

--- script_2.rb
+++ script_2.rb
@@ -6,3 +6,21 @@ redact = gets.chomp
 
 words = text.split(" ")
 words.each { |word| print word }
+
+print "What's your first name?"
+first_name = gets.chomp
+first_name.capitalize
+
+print "What's your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+print "Whats your city?"
+city = gets.chomp
+city.capitalize!
+
+print "Whats your state?"
+state = gets.chomp
+state.upcase!
+
+print "My name is #{first_name} #{last_name}. I live is #{city} and #{state}!"
