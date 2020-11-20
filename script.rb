850b6701ff4262cb447c3ae497d801c500436b79
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-19 18:51:27 -0800

    control flow in ruby example

--- script.rb
+++ script.rb
@@ -12,3 +12,14 @@ words.each do |word|
     print "REDACTED "
   end
 end
+
+
+print "Please input your string:"
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+   puts user_input.gsub!(/s/, "th")
+else
+   puts "No ss found!"
+end
