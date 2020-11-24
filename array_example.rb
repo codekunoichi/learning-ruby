8ca36388c1b8177fddf78b0564ea914adb825c6f
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-24 11:18:18 -0800

    2d array example with .each iterating over the array elements

--- array_example.rb
+++ array_example.rb
@@ -10,3 +10,7 @@ puts numbers
 
 mixed_arr = ["oranges", 1, 3.0, true]
 puts mixed_arr
+
+my_2d_array = [["oranges", "apples", "kiwi"], [1, 2, 3], [true, false, true], [3.3, 4.2, 5.2]]
+
+my_2d_array.each{ |x| puts "#{x}\n"}
