c8dab64a626018396eb5337a95c844405d92649c
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-24 11:14:50 -0800

    add more array examples

--- array_example.rb
+++ array_example.rb
@@ -1,3 +1,12 @@
 multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
 multi_d_array.each { |x| puts "#{x}\n" }
+
+words = ["oranges", "apples", "kiwi"]
+puts words
+
+numbers = [1, 2, 3, 4]
+puts numbers
+
+mixed_arr = ["oranges", 1, 3.0, true]
+puts mixed_arr
