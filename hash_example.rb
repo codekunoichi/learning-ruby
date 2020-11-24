5974aa867f8a115cae2e9c4ded9945b4deae7b3e
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-24 11:26:50 -0800

    Add example for empty hash creation

--- hash_example.rb
+++ hash_example.rb
@@ -1,3 +1,4 @@
+# Hash Literal Notation
 my_hash = {
   "name" => "codekunoichi",
   "age" => 90,
@@ -7,3 +8,15 @@ my_hash = {
 puts my_hash["age"]
 puts my_hash["name"]
 puts my_hash["gender"]
+
+# Setting a variable equal to Hash.new creates a new, empty hash;
+# it’s the same as setting the variable equal to empty curly braces ({}).
+
+pets = Hash.new
+
+pets["Jane"] = "dog"
+pets["boomba"] = "dog"
+pets["pinki"] = "gerbil"
+pets["chutki"] = "rabbit"
+
+puts pets
