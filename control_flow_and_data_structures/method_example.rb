6833064902891388316387d8a028a9b5fa6fd2bb
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-12-04 14:53:13 -0800

    add additional puts to separate sections to enhance  readability of the output

--- control_flow_and_data_structures/method_example.rb
+++ control_flow_and_data_structures/method_example.rb
@@ -15,11 +15,43 @@ def  divide(one, two)
   one / two
 end
 
+# predicate method
+def can_divide_by?(number)
+  return false if number.zero?
+  true
+end
+
+
+def factorial(n)
+  n == 0 ? 1 : n * factorial(n-1)
+end
+
+def factorial_with_default(n = 5)
+  n == 0 ? 1 : n * factorial_with_default(n-1)
+end
+
+puts "**************** method example with default arguments"
+puts factorial 3
+puts factorial_with_default
+puts factorial_with_default 4
+
+puts "**************** splat_example"
+
+def splat_example(param_one, *num, param_last)
+  num.max
+end
+
+puts splat_example("first_one", 1, 34, 22, 99, 100)
+puts "**************** method examples with parenthesis and without"
 sample
 sample()
 sample1()
 sample1
-
+puts "**************** sample method invocation"
 puts add(1, 2)
 puts divide(3, 0)
 puts divide(12, 3)
+puts "**************** predicate method example"
+puts can_divide_by? 0
+puts can_divide_by? 3
+puts "****************"
