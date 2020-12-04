51f6ab52016a26843d1421025ae3a8f1b3fe868e
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-12-04 08:27:24 -0800

    boolean example

---
+++ control_flow_and_data_structures/boolean_example.rb
@@ -0,0 +1,8 @@
+# only false , nil is false, everything else is true
+
+puts  "0 is true" if 0
+puts  "false  string is not false" if "false"
+puts  "only false is false" if false
+puts  "nil string is not false" if "nil"
+puts  "empty string is true" if ""
+puts  "nil is false" if nil
