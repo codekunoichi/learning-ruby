39a397402b5545260abb58bdad9f301e76edd77c
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-12-14 16:37:07 -0800

    Array manipulation and predicate method practice.

--- Excercism/ruby/high-scores/high_scores.rb
+++ Excercism/ruby/high-scores/high_scores.rb
@@ -9,14 +9,36 @@ To get started with TDD, see the `README.md` file in your
 class HighScores
   attr_reader :scores
   attr_reader :latest
-  attr_reader :personal_best
   def initialize(scores)
     @scores = scores
     @latest  = @scores.last
-    @personal_best = @scores.max
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    sorted_ar = @scores.sort.reverse
+    length_of_arr = sorted_ar.length
+    if length_of_arr >= 3
+      sorted_ar[0..2]
+    else
+      sorted_ar[0..length_of_arr]
+    end
+  end
+
+  def latest_is_personal_best?
+    @latest == personal_best
   end
 end
 
 hs = HighScores.new([30, 50, 20, 70])
 p hs.scores
+puts "Is latest personal best? #{hs.latest_is_personal_best?}"
 puts "\n Latest = #{hs.latest}"
+hs2 = HighScores.new([10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70])
+puts "\n personal top three = #{hs2.personal_top_three}"
+
+hs3 = HighScores.new([30, 70])
+puts "\n personal top three with less than three aray size = #{hs3.personal_top_three}"
