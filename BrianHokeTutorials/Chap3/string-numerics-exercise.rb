22bfcb78a1cdc41ec10a5711e7ca8e0d2c1c3b47
Rumpa Giri <giri.rumpa2017@gmail.com>
2021-03-07 15:26:05 -0800

    String Numeric example code

---
+++ BrianHokeTutorials/Chap3/string-numerics-exercise.rb
@@ -0,0 +1,41 @@
+# 1. test if a string is a palindrome
+class String
+	def palindrome?
+		# your code here
+		self == self.reverse
+	end
+end
+
+str1 = "able was I ere I saw elba"
+str2 = "I ain't no palindrome"
+# should print "str1 is a palindrome: true"
+puts "str1 is a palindrome: #{str1.palindrome?}"
+# should print "str2 is a palindrome: false"
+puts "str2 is a palindrome: #{str2.palindrome?}"
+
+
+# 2. count number of vowels in a string
+class String
+	def num_vowels
+		self.downcase.count "aeiou"
+	end
+end
+
+str1 = "Joe Appleby"
+# should print "str1 has 4 vowels"
+puts "'#{str1}' has #{str1.num_vowels} vowel(s)"
+
+
+# 3. method to reduce fractions
+#    hint: use
+
+def reduceit(num,den)
+	# your code here
+	gcd = num.gcd(den)
+	return "#{num}/#{den} = #{num/gcd}/#{den/gcd}"
+end
+
+puts reduceit(4,24)
+puts reduceit(5,10)
+puts reduceit(12,3)
+puts reduceit(5,7)
